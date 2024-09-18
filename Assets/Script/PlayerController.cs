c26b09f7baecc003679766667c8f865d8cfb8c2f
Huu Tai <phamhuutai200254321@gmail.com>
2024-09-18 19:52:31 +0700

    animating Player

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Horizontal Movement Settings")]
-    private Rigidbody2D rb;
-
     [SerializeField] private float walkSpeed = 1;
-
-    private float xAxis;
 
     [Header("Ground Check Settings")]
     [SerializeField] private float jumpForce = 45;
-
     [SerializeField] private Transform groundCheckPoint;
-
     [SerializeField] private float groundCheckY = 0.2f;
     [SerializeField] private float groundCheckX = 0.5f;
-
     [SerializeField] private LayerMask whatIsGround;
+
+    private Rigidbody2D rb;
+    private float xAxis;
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,6 +32,7 @@ public class PlayerController : MonoBehaviour
         GetInputs();
         Move();
         Jump();
+        Flip();
     }
 
     void GetInputs()
@@ -39,9 +40,23 @@ public class PlayerController : MonoBehaviour
         xAxis = Input.GetAxisRaw("Horizontal");
     }
 
+    void Flip()
+    {
+        if (xAxis < 0)
+        {
+            transform.localScale = new Vector2(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+        else if (xAxis > 0)
+        {
+            transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+    }
+
     private void Move()
     {
         rb.velocity = new Vector2(walkSpeed * xAxis, rb.velocity.y);
+
+        anim.SetBool("Walking", rb.velocity.x != 0 && Grounded());
     }
 
     public bool Grounded()
@@ -60,9 +75,15 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
         if (Input.GetButtonDown("Jump") && Grounded())
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpForce);
         }
+
+        anim.SetBool("Jumping", !Grounded());
     }
 }
